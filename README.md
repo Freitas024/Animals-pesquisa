ae0055cf933fdb07f8d33b79c3a36ed5c80b0d8b
vinicius Freitas <viniciusfreitassilva1999@gmail.com>
2024-09-07 12:38:46 -0300

    re-dsigner do site e adicionando mais resultados

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
-<h1>Atividade da faculdade de ADS.</h1>
-<h2>Atividade em HTML, CSS e JAVASCRIPT puro.</h2>
-<h3>Página web de aplicação de filtragem de card.</h3>
+<h2>Pesquisa de Animais</h2>
+<hr>
+<p>Aplicação web de um website que faz a filtragem de animais por palavras chaves ultilizando somente HTML, CSS e JAVASCRIPT</p>
 
-<p>Venha me seguir no github: <a href="https://github.com/Freitas024">@Freitas024</a></p>
+<h2>paletas de cores</h2>
+<li>Codigo da cor: <p>#ca7446</p></li>
+<li>Codigo da cor: <p>#faedcd</p></li>
+<li>Codigo da cor: <p>#cb997e</p></li>
+<li>Codigo da cor: <p>#6c584c</p></li>
+<li>Codigo da cor: <p>#b5c99a</p></li>
+<li>Codigo da cor: <p>#e9f5db</p></li>
+<li>Codigo da cor: <p>#FFFFFF</p></li>
+
+<p>Venha vê outros projetos meus <a href="https://github.com/Freitas024">@Freitas024</a></p>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,39 @@
 let dados = [
     {
         titulo: "Gato",
-        descricao: "fofo gatinho gato laranja peludo",
-        link: "../public/cat.jpg"
+        descricao: "O gato ou gato doméstico é um mamífero carnívoro da família dos felídeos, muito popular como animal de estimação. Ocupando o topo da cadeia alimentar, é predador natural de diversos animais, como roedores, pássaros, lagartixas e alguns insetos.",
+        link: "../public/cat.jpg",
+        tag: "gato cat gatinho mamifero terrestre"
     },
     {
         titulo: "Cachorro",
-        descricao: "cachorro dog chorro peludo",
-        link: "../public/dog.jpg"
+        descricao: "O cão, no Brasil também chamado de cachorro, é um mamífero carnívoro da família dos canídeos, subespécie do lobo, e talvez o mais antigo animal domesticado pelo ser humano. Teorias postulam que surgiu do lobo cinzento no continente asiático há mais de 100 000 anos.",
+        link: "../public/dog.jpg",
+        tag: "cachorro cao dog cachorrinho mamifero terrestre"
     },
     {
         titulo: "Cobra",
-        descricao: "Cobra cobra serpente",
-        link: "../public/cobra.jpg"
+        descricao: "As serpentes, também chamadas ofídios, cobras, mbóis, mboias e malacatifas, são répteis poiquilotérmicos sem patas, pertencentes à subordem Serpentes, ou Ophidia. São bastante próximos dos lagartos, com os quais partilham a ordem Squamata.",
+        link: "../public/cobra.jpg",
+        tag: "serpente cobra vibora rastegante terrestre"
+    }, 
+    {
+        titulo: "Passaro",
+        descricao: "Aves são uma classe de seres vivos vertebrados endotérmicos caracterizada pela presença de penas, um bico sem dentes, oviparidade de casca rígida, elevado metabolismo, um coração com quatro câmaras e um esqueleto pneumático resistente e leve. ",
+        link: "../public/passaro.jpg",
+        tag: "ave passsaro passarinho voador"
+    },
+    {
+        titulo: "peixe",
+        descricao: "Os Peixes são animais vertebrados, aquáticos, tipicamente ectotérmicos, que possuem o corpo fusiforme, os membros transformados em barbatanas ou nadadeiras sustentadas por raios ósseos ou cartilaginosos.",
+        link: "../public/peixe.jpg",
+        tag: "marinho peixe agua fish nadador"
+    },
+    {
+        titulo: "Lontra",
+        descricao: "A subfamília Lutrinae pertence à família dos Mustelídeos. Existem 13 espécies, dentro de 7 géneros. Todas as espécies são nadadoras, vivendo no litoral, em rios ou em lagos, e possuem os dedos unidos por membranas interdigitais, que facilitam a natação.",
+        link: "../public/lontra.jpg",
+        tag: "lontra mamifero nadador terrestre"
     }
 ];
 
@@ -37,18 +58,22 @@ function pesquisar(){
 
     let resultado = "";
     let titulo = "";
-    let descricao = "";
+    let tag = "";
     
 
     for (let dado of dados){
         titulo = dado.titulo.toLowerCase();
         descricao = dado.descricao.toLowerCase();
-        if(titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa)){
+        tag = dado.tag.toLowerCase();
+
+        if(titulo.includes(campoPesquisa) || tag.includes(campoPesquisa)){
             resultado += `
         <div class="card">
-            <img src="${dado.link}" style="width: 100%;"/>            
-            <h3 class="title">${dado.titulo}</h3> 
-            <p class="text">${dado.descricao}</p>
+            <img src="${dado.link}" class="card-image" "/>  
+            <div class="card-information">
+                <h3 class="card-information-title">${dado.titulo}</h3> 
+                <p class="card-information-text">${dado.descricao}</p>
+            </div>          
         </div>
         
         `;
