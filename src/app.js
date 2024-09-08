01a41eceb0532d0a19999ea41341aed378b99181
vinicius Freitas <viniciusfreitassilva1999@gmail.com>
2024-09-08 12:10:09 -0300

    pesquisa de animais

--- src/app.js
+++ src/app.js
@@ -2,37 +2,37 @@ let dados = [
     {
         titulo: "Gato",
         descricao: "O gato ou gato doméstico é um mamífero carnívoro da família dos felídeos, muito popular como animal de estimação. Ocupando o topo da cadeia alimentar, é predador natural de diversos animais, como roedores, pássaros, lagartixas e alguns insetos.",
-        link: "../public/cat.jpg",
+        link: "https://images.unsplash.com/photo-1472491235688-bdc81a63246e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         tag: "gato cat gatinho mamifero terrestre"
     },
     {
         titulo: "Cachorro",
         descricao: "O cão, no Brasil também chamado de cachorro, é um mamífero carnívoro da família dos canídeos, subespécie do lobo, e talvez o mais antigo animal domesticado pelo ser humano. Teorias postulam que surgiu do lobo cinzento no continente asiático há mais de 100 000 anos.",
-        link: "../public/dog.jpg",
+        link: "https://images.unsplash.com/photo-1422565096762-bdb997a56a84?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         tag: "cachorro cao dog cachorrinho mamifero terrestre"
     },
     {
         titulo: "Cobra",
         descricao: "As serpentes, também chamadas ofídios, cobras, mbóis, mboias e malacatifas, são répteis poiquilotérmicos sem patas, pertencentes à subordem Serpentes, ou Ophidia. São bastante próximos dos lagartos, com os quais partilham a ordem Squamata.",
-        link: "../public/cobra.jpg",
+        link: "https://images.unsplash.com/photo-1539389004540-770367e1bccb?q=80&w=2034&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         tag: "serpente cobra vibora rastegante terrestre"
     }, 
     {
         titulo: "Passaro",
         descricao: "Aves são uma classe de seres vivos vertebrados endotérmicos caracterizada pela presença de penas, um bico sem dentes, oviparidade de casca rígida, elevado metabolismo, um coração com quatro câmaras e um esqueleto pneumático resistente e leve. ",
-        link: "../public/passaro.jpg",
+        link: "https://images.unsplash.com/photo-1486365227551-f3f90034a57c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         tag: "ave passsaro passarinho voador"
     },
     {
         titulo: "peixe",
         descricao: "Os Peixes são animais vertebrados, aquáticos, tipicamente ectotérmicos, que possuem o corpo fusiforme, os membros transformados em barbatanas ou nadadeiras sustentadas por raios ósseos ou cartilaginosos.",
-        link: "../public/peixe.jpg",
+        link: "https://images.unsplash.com/photo-1522069169874-c58ec4b76be5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZmlzaHxlbnwwfHwwfHx8MA%3D%3D",
         tag: "marinho peixe agua fish nadador"
     },
     {
         titulo: "Lontra",
         descricao: "A subfamília Lutrinae pertence à família dos Mustelídeos. Existem 13 espécies, dentro de 7 géneros. Todas as espécies são nadadoras, vivendo no litoral, em rios ou em lagos, e possuem os dedos unidos por membranas interdigitais, que facilitam a natação.",
-        link: "../public/lontra.jpg",
+        link: "https://images.unsplash.com/photo-1633967920376-33b2d94f091f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGxvbnRyYXxlbnwwfHwwfHx8MA%3D%3D",
         tag: "lontra mamifero nadador terrestre"
     }
 ];
